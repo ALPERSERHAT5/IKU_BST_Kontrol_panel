f1f4fd699b2a7bf29337dc135f4c9063e87c49c5
Serhat Alper <senin@iku.edu.tr>
2026-09-24 22:22:59 +0300

    backend url guncellendi

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:4000/api';
+const API_BASE = 'https://bst-backend.onrender.com/api';
 
 export function getAccessToken() {
   return localStorage.getItem('accessToken');
